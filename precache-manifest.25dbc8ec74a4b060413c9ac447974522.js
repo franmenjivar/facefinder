32677a12f2c68110f9b26b771c4200c16ddef81a
franmenjivar <199fran5@gmail.com>
2020-08-20 23:59:09 -0600

    Updates

---
+++ precache-manifest.25dbc8ec74a4b060413c9ac447974522.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0398d27ed4e2f9248e3766969cd92593",
+    "url": "/facefinder/index.html"
+  },
+  {
+    "revision": "3a558d3b7e17116fbf15",
+    "url": "/facefinder/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ca81b75517c3265ca052",
+    "url": "/facefinder/static/css/main.ea3d4f88.chunk.css"
+  },
+  {
+    "revision": "3a558d3b7e17116fbf15",
+    "url": "/facefinder/static/js/2.7898302f.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facefinder/static/js/2.7898302f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca81b75517c3265ca052",
+    "url": "/facefinder/static/js/main.122185c2.chunk.js"
+  },
+  {
+    "revision": "e5a337749c5a7b6c5d34",
+    "url": "/facefinder/static/js/runtime-main.40c10fa7.js"
+  },
+  {
+    "revision": "e20302d364f9f73a5b76f4b64b1bbb76",
+    "url": "/facefinder/static/media/facelogo.e20302d3.jpg"
+  }
+]);
