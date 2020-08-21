7117254e5583c9e5185975804c6198f9f50af03f
franmenjivar <199fran5@gmail.com>
2020-08-21 00:24:14 -0600

    Adding Files

---
+++ src/components/Signin/Signin.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+
+const Signin =({onRouteChange}) =>{
+    return(
+        <article className='br3 pa2 ba  white mv4 w-100 w-50-m w-25-l mw6 shadow-5 center'>
+        <main className="pa4 black-80">
+            <div className="measure ">
+                <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                <legend className=" white f1 fw6 ph0 mh0">Sign In</legend>
+                <div className="mt3 ">
+                    <label className="white db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                    <input className="pa2 white input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
+                </div>
+                <div className="mv3">
+                    <label className=" white db fw6 lh-copy f6" htmlFor="password">Password</label>
+                    <input className="b pa2 white input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
+                </div>
+                
+            </fieldset>
+            <div className="">
+      <input 
+      onClick={()=> onRouteChange('home')}
+      className="b ph3 pv2 input-reset ba white bg-transparent grow pointer f6 dib" 
+      type="submit" 
+      value="Sign in"/>
+        </div>
+    <div className="lh-copy mt3 pointer">
+      <p onClick={()=> onRouteChange('register')} className="f6 link dim white db">Register</p>
+      
+    </div>
+  </div>
+</main>
+</article>
+
+    )
+
+}
+
+
+export default Signin;
+
